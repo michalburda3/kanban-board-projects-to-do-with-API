154c1822a0893a1c7d1cfbcd1e3edfc9d20c0cf9
Michal <michal.burda3@gmail.com>
2016-12-26 19:41:55 +0100

    added files

---
+++ app.js
@@ -0,0 +1,35 @@
+// AJAX communication
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+	"X-Client-Id": '77',
+	"X-Auth-Token": '076f6323443c30469eee2cb9bf5faf65'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+		});
+  };
+
+
